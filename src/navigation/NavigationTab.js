6e3281a060437a45b66f669ec93649a447f5748b
Jaime <jaimeibarra.ce@gmail.com>
2024-08-07 20:42:50 -0500

    Iconos
    se agregan iconos a la navegacion

--- src/navigation/NavigationTab.js
+++ src/navigation/NavigationTab.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import Account from '../screens/Account';
 import Favourite from '../screens/Favourite';
 import Pokedex from '../screens/Pokedex';
@@ -9,9 +10,23 @@ const Tab = createBottomTabNavigator();
 export default function NavigationTab(props) {
   return (
     <Tab.Navigator initalRouteName="Account">
-        <Tab.Screen name="Account" component={Account} />
+        <Tab.Screen 
+            name="Account" 
+            component={Account} 
+            options={{
+                tabBarLabel: "Mi Cuenta",
+                tabBarIcon: (color, size) => <Icon name="user" color={color} size={size}/>,
+            }} 
+        />
         <Tab.Screen name="Pokedex" component={Pokedex} />
-        <Tab.Screen name="Favourite" component={Favourite} />
+        <Tab.Screen 
+            name="Favourite" 
+            component={Favourite} 
+            options={{
+                tabBarLabel: "Favoritos", 
+                tabBarIcon: (color, size) => <Icon name="heart" color={color} size={size}/>,
+            }}
+        />
     </Tab.Navigator>
   )
 }
